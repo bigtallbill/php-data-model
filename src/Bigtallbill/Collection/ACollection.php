8894b483192c5502d197ea850d6bf92878b41dbf
bigtallbill <bill@bigtallbill.co.uk>
2015-01-08 21:01:17 -0500

    Add constructor and abstract methods

--- src/Bigtallbill/Collection/ACollection.php
+++ src/Bigtallbill/Collection/ACollection.php
@@ -11,11 +11,43 @@ namespace Bigtallbill\Collection;
 
 use Bigtallbill\Interfaces\IDbWrapperUser;
 use Bigtallbill\Model\DbWrapper\ADbWrapper;
+use Bigtallbill\Model\DbWrapper\OperationOptions;
 
 abstract class ACollection implements IDbWrapperUser, \ArrayAccess, \Iterator, \Countable
 {
     /** @var ADbWrapper */
     protected $conn;
+    protected $databaseName;
+    protected $collectionName;
+
+    /**
+     * @param ADbWrapper $client
+     * @param string $databaseName
+     * @param string $collectionName
+     */
+    function __construct(
+        ADbWrapper $client = null,
+        $databaseName = '',
+        $collectionName = ''
+    ) {
+        $this->setConnection($client);
+        $this->databaseName = $databaseName;
+        $this->collectionName = $collectionName;
+    }
+
+    /**
+     * Should populate this collection with results of the query
+     *
+     * @param OperationOptions $options
+     */
+    abstract public function find(OperationOptions $options);
+
+    /**
+     * Should batch insert all objects in this collection
+     *
+     * @param OperationOptions $options
+     */
+    abstract public function insert(OperationOptions $options);
 
     //--------------------------------------
     // INTERFACE REQUIREMENTS
